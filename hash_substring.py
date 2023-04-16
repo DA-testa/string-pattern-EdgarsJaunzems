a16163651d1da0d75c48b1cd1743cc5ae2ba74a2
EdgarsJaunzems <124889727+EdgarsJaunzems@users.noreply.github.com>
2023-04-16 20:57:43 +0300

    d

--- hash_substring.py
+++ hash_substring.py
@@ -3,28 +3,43 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    inp = input()
+    if "I" in inp
+        rinda1=input().strip()
+        rinda2=input().strip()
+        return(rinda1,rinda2)
+    if "F" in inp
+        with open('./tests/06') as test:
+            t1=test.readline().strip()
+            t2=test.readline().strip()
+        return (t1,t2)
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
+def get_occurrences(pat, text):
     # this function should find the occurances using Rabin Karp alghoritm 
 
-    # and return an iterable variable
-    return [0]
+    p = len(pat)
+    t = len(text)
 
+    oc = []
+
+    phash = hash(pat)
+    thash = hash(text[:p])
+
+    for i in range(t-p+1):
+        if phash == thash and text[i:i+p] == pat:
+            oc.append(i)
+
+        if i < t-p:
+            thash = hash(text[1+i:1+p+i])
+    
+
+    # and return an iterable variable
+    return oc
 
 # this part launches the functions
 if __name__ == '__main__':
